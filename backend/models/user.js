5c8a1c6df86d2589857278c87a24039512613ff8
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-10 21:34:57 -0300

    add bcrypt configs

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 let userSchema = new mongoose.Schema({
   name: String,
@@ -11,6 +12,19 @@ let userSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
+});
+
+userSchema.pre("save", (next) => {
+  if (this.isNew || this.isModified("password")) {
+    const document = this;
+    bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+      if (err) next(err);
+      else {
+        this.password = hashedPassword;
+        next();
+      }
+    });
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
