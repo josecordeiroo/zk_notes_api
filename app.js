cfa53d3627d32ced65079f3c1fbb48b5b858cadc
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-14 18:15:32 -0300

    cors add

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var logger = require('morgan');
 require('./config/database')
 
+var cors = require('cors')
+
 //Routes
 var usersRouter = require('./app/routes/users');
 var notesRouter = require('./app/routes/notes');
@@ -13,6 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
